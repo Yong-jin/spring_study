99d2184558f890c8cc0e7934affe9c5cfca659e1
Yong-jin <takeardor@gmail.com>
2026-02-04 12:42:47 +0900

    관리자페이지 전체 객실 목록 조회하기 추가

--- SpringStudy/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ SpringStudy/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -27,8 +27,9 @@ public class RoomDAOImpl implements RoomDAO {
 		System.out.println("[DAO] 호출 findRoomList");
 		
 		// DB 에 연동 -> room 테이블 데이터 조회 -> List
+		List<Room> roomList = sqlSessionTemplate.selectList("room_mapper.findRoomList");
 		
-		return null;
+		return roomList;
 	}
 
 	@Override
