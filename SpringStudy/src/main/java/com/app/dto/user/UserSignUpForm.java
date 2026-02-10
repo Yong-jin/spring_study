e0af6027bcd0e554aed2ef9afc5c3c2646661ecc
Yong-jin <takeardor@gmail.com>
2026-02-10 16:25:14 +0900

    유효성 검증 2 (DTO객체 어노테이션 방식)

--- SpringStudy/src/main/java/com/app/dto/user/UserSignUpForm.java
+++ SpringStudy/src/main/java/com/app/dto/user/UserSignUpForm.java
@@ -6,18 +6,18 @@ import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class User {
+public class UserSignUpForm {
 
-	//필수입력 빈칸X
-	@NotBlank(message= "아이디 필수임")
+	// 필수입력 빈칸X
+	@NotBlank(message = "아이디 필수임")
 	String id;
-	
-	//비번 길이제한
+
+	// 비번 길이제한
 	@Size(min = 8, max = 12, message = "비번 길이 확인!!!")
 	String pw;
 	String name;
 	String userType;
-	//사용자 계정 구분
-	//Customer	Admin
-	//     CUS	ADM
+	// 사용자 계정 구분
+	// Customer Admin
+	// CUS ADM
 }
